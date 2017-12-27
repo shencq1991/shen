85ab5413103daf8c59a34ba34aed2bdb71a2fddc
shencq1991 <419394289@qq.com>
2017-12-27 08:51:09 +0800

    sx

--- cmdb/admin.py
+++ cmdb/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_time']
-
-admin.site.register(Post, PostAdmin)
+# from .models import Post
+#
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'created_time']
+#
+# admin.site.register(Post, PostAdmin)
--- cmdb/views.py
+++ cmdb/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from cmdb.models import Post
-from cmdb.models import Echarts
+# from cmdb.models import Post
+# from cmdb.models import Echarts
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import markdown
@@ -30,7 +30,7 @@ def data(request):
 
 
 def content(request,pk):
-    content = get_object_or_404(Post, pk=pk)
+    # content = get_object_or_404(Post, pk=pk)
     # 展示的是第PK条数据（一条记录）
     # content.body = markdown.markdown(content.body,
     #                               extensions=[
@@ -40,8 +40,7 @@ def content(request,pk):
     #                               ])
     # data = get_object_or_404(Echarts,pk=pk)
 
-    return render(request,"content.html",{'content':content
-                                          })
+    return render(request,"content.html",)
 
 
 def ie(request):
@@ -49,26 +48,25 @@ def ie(request):
 
 
 def web_1(request):
-    data = Echarts.objects.all()
 
-    return render(request,"web_1.html",{'data':data})
+    return render(request,"web_1.html",)
 
 
 
 
 def web_blog(request):
-    limit = 4
-    post = Post.objects.all().order_by('id')
-    p = Paginator(post,limit)
-    page = request.GET.get('page')
-    try:
-        contents = p.page(page)
-    except PageNotAnInteger:
-        contents = p.page(1)
-    except EmptyPage:
-        contents = p.page(paginator.num_pages)
+    # limit = 4
+    # post = Post.objects.all().order_by('id')
+    # p = Paginator(post,limit)
+    # page = request.GET.get('page')
+    # try:
+    #     contents = p.page(page)
+    # except PageNotAnInteger:
+    #     contents = p.page(1)
+    # except EmptyPage:
+    #     contents = p.page(paginator.num_pages)
+    #
+    #
 
-
-
-    return render(request,"web_blog.html",{'contents':contents})
+    return render(request,"web_blog.html",)
 
