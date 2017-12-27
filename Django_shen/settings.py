2e61d5fa1d62004c5be669213a9dc5999811f731
shencq1991 <419394289@qq.com>
2017-12-27 11:17:12 +0800

    x

--- Django_shen/settings.py
+++ Django_shen/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = '#@y4sz=dtmtz@w76o!fzu4t*x@i66^^=afe!vsol102*6@wh!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','.workm-station.com']
+ALLOWED_HOSTS = ['127.0.0.1','39.108.156.176','.workm-station.com']
 
 
 # Application definition
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 DATABASES = {
